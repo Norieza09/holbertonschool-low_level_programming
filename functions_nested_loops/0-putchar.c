6bcd13083ef0d24907093242099fceceb6447ced
Noriexie <9474@holbertonstudents.com>
2024-06-19 09:39:26 -0400

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,18 +2,15 @@
 
 /**
  * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
