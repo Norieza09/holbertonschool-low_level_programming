6720ce6d95346b6f8d8a8193eeadae700c3dfa3a
Noriexie <9474@holbertonstudents.com>
2024-06-19 10:14:30 -0400

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char text[] = "_putchar\n";
-    for (int i = 0; text[i] != '\0'; i++) {
-        write(1, &text[i], 1);
-    }
-    return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
