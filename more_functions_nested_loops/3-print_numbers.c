bc2b8645a523dbd52b68e91fd8c1e81e99f6fe6c
Noriexie <9474@holbertonstudents.com>
2024-06-20 12:58:19 -0400

    numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-#include "_putchar.c"
 
 /**
 *print_numbers - prints the numbers, from 0 to 9, followed by a new line.
-*Return: Always 0
+*Return: Always 0.
 */
 
 void print_numbers(void)
