6347ca970608c01315485cea72fa7f5139a8a80b
Noriexie <9474@holbertonstudents.com>
2024-06-20 10:38:32 -0400

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiples two integers
+ * @a: the variable to use
+ * @b: the variable to use
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
