aad6b4222a2b0347d1de8bc5997949469983d665
Noriexie <9474@holbertonstudents.com>
2024-06-20 13:17:11 -0400

    numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * most numbers- prints the numbers, from 0 to 9, followed by a new line.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+{
+	if (c != '2' && c != '4')
+	{
+		_putchar(c);
+	}
+	c++;
+}
+_putchar('\n');
+}
