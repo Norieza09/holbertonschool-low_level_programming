7c4b472291145b7b5ca9a3a2f7615254e414ceda
Noriexie <9474@holbertonstudents.com>
2024-07-10 11:52:48 -0400

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - freeing dogs
+ * @d: to free the dog
+ *
+ */
+
+void free_dog(dog_t *d)
+
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
